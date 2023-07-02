c36513b330a8083c7029a1f9eac9a1072709eebb
LHY <15897476550@163.com>
2023-07-02 21:42:24 +0800

    子弹类

--- games/alien_invasion/settings.py
+++ games/alien_invasion/settings.py
@@ -10,8 +10,17 @@ class Settings:
         # self.is_full_scree = True
 
         self.bg_color = (230, 230, 230)
-        self.caption = '外星人入侵'
-        self.step = 100
 
+        self.caption = '外星人入侵'
+
+        # 移动速度
+        self.step = 100
+        # 大小
         self.step_width = 50
         self.step_height = 50
+
+        # 子弹设置
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
