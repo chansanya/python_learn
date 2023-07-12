f44d702d252c00731e8d456ee2b7a53850c391ea
lhy <984038622@qq.com>
2023-07-12 10:31:39 +0800

    地震散点图

--- games/matplotlib/eq_explore_data.py
+++ games/matplotlib/eq_explore_data.py
@@ -1,4 +1,7 @@
 import json
+import plotly.express as px
+import pandas as pd
+
 # 探索数据的结构。
 filename = 'source/eq_data_1_day_m1.json'
 with open(filename) as f:
@@ -6,3 +9,40 @@ with open(filename) as f:
 
 all_eq_dicts = all_eq_data['features']
 print(len(all_eq_dicts))
+
+
+mags, titles, lons, lats = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    title = eq_dict['properties']['title']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+
+    mags.append(mag)
+    titles.append(title)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags[:10])
+
+
+
+data = pd.DataFrame(
+    data=zip(lons, lats, titles, mags), columns=["经度", "纬度", "位置", "震级"]
+)
+data.head()
+fig = px.scatter(
+    data,
+    x="经度",
+    y="纬度",
+    range_x=[-200, 200],
+    range_y=[-90, 90],
+    width=800,
+    height=800,
+    title="全球地震散点图",
+    size="震级",
+    size_max=10,
+    color="震级"
+)
+fig.write_html("./html/global_earthquakes.html")
+fig.show()
